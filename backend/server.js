ddb21190ca41ef43c5ee235bfd60f72b13bbaf37
shayan-webdeveloper <shayanadec2@gmail.com>
2026-09-04 11:38:15 +0500

    Task Done

--- backend/server.js
+++ backend/server.js
@@ -41,8 +41,7 @@ app.get("/api/protected", authMiddleware, (req, res) => {
     user: req.user,
   });
 });
-mongoose
-  .connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("MongoDB connected successfully");
 
